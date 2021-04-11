cc34a4116b27332183d67717843878ca8775ace5
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-04-11 10:38:59 +0200

    simple junit test

--- src/main/java/PixelDemo.java
+++ src/main/java/PixelDemo.java
@@ -1,6 +1,7 @@
 public class PixelDemo {
     public static void main(String[] args) {
         Pixel pixel = new Pixel(3,4);
-        System.out.println(pixel.getSum());
+        int result = pixel.getSum();
+        System.out.println(result);
     }
 }
--- src/test/java/PixelTest.java
+++ src/test/java/PixelTest.java
@@ -5,8 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class PixelTest {
 
     @Test
-    public void testPixel(){
+    public void testPixel() {
+        //given - przygotowanie
+        Pixel pixel = new Pixel(2, 3);
 
+        //when - sytuacja którą sprawdzamy
+        int sum = pixel.getSum();
+
+        //than - czy efekt jest taki jak oczekiwaliśmy
+        assertEquals(5,sum);
     }
 
 }
